d859b28237557608de4445c2a5ef0deaa94778aa
Federico <admin@imgdigital.com.ar>
2017-01-09 11:14:00 -0300

    New Favicon

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
+<link rel="icon" type="image/png" href="http://vision.travel/assets/images/favicon.png" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
