ecc7721634fb9139fbf5b746d9bd33c34a065a6f
Federico <admin@imgdigital.com.ar>
2017-01-19 16:23:38 -0300

    esconder shortcodes

--- imgd/imgd_shortcode.php
+++ imgd/imgd_shortcode.php
@@ -1,6 +1,3 @@
 <?php
 
-function imgd_rockthemes_specialgridblock( $atts, $content = null ) {
-    return $content;
-}
-add_shortcode( 'rockthemes_specialgridblock', 'imgd_rockthemes_specialgridblock' );
+add_shortcode( 'rockthemes_specialgridblock', '__return_false' );
