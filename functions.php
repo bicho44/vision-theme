13bd7232c9ae798250ffe64ce51a0ffbb1762be4
Federico <admin@imgdigital.com.ar>
2017-03-16 10:59:14 -0300

    Add Link support

--- functions.php
+++ functions.php
@@ -59,6 +59,8 @@ function turismointer_setup() {
 		'caption',
 	) );
 
+	add_theme_support( 'post-formats', array( 'link' ) );
+
 	// Set up the WordPress core custom background feature.
 	/*add_theme_support( 'custom-background', apply_filters( 'turismointer_custom_background_args', array(
 		'default-color' => 'ffffff',
